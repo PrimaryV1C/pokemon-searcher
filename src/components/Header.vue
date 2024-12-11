6f529bd233948d1eceaab7d5dec63a8aeef1215b
PrimaryV1C <petrovictorelias@gmail.com>
2024-12-11 09:48:39 +0100

    Created Header.vue

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <header>
+    <div>
+      <img src="./src/assets/logo.svg" alt="Logo" />
+      <nav>
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Contact</a>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
