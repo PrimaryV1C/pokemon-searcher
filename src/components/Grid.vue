a2d61d3ca8ca6af6de97c6123f2248ee70cae1ec
PrimaryV1C <petrovictorelias@gmail.com>
2024-12-11 11:44:03 +0100

    Added dummy data

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -18,6 +18,54 @@
 <script>
 export default {
   name: "GridComponent",
+  data() {
+    return {
+      gridItems: [
+        {
+          id: 1,
+          image: "./src/assets/profile-images/mario.webp",
+          title: "Mario",
+          text: "The heroic plumber from the Mushroom Kingdom.",
+          url: "https://www.nintendo.com",
+        },
+        {
+          id: 2,
+          image: "./src/assets/profile-images/link.webp",
+          title: "Link",
+          text: "The brave adventurer from The Legend of Zelda.",
+          url: "https://www.nintendo.com",
+        },
+        {
+          id: 3,
+          image: "./src/assets/profile-images/samus.webp",
+          title: "Samus Aran",
+          text: "The legendary bounty hunter from Metroid.",
+          url: "https://www.nintendo.com",
+        },
+        {
+          id: 4,
+          image: "./src/assets/profile-images/kirby.png",
+          title: "Kirby",
+          text: "The pink puffball who can inhale anything.",
+          url: "https://www.nintendo.com",
+        },
+        {
+          id: 5,
+          image: "./src/assets/profile-images/pikachu.png",
+          title: "Pikachu",
+          text: "The iconic Electric-type Pokémon.",
+          url: "https://www.nintendo.com",
+        },
+        {
+          id: 6,
+          image: "./src/assets/profile-images/dk.webp",
+          title: "Donkey Kong",
+          text: "The strong and lovable leader of the DK crew.",
+          url: "https://www.nintendo.com",
+        },
+      ],
+    };
+  },
   methods: {
     navigateTo(url) {
       window.location.href = url;
