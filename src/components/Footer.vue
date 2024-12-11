fd83643bdb5020be578b8e2ee28909e47546a4ca
PrimaryV1C <petrovictorelias@gmail.com>
2024-12-11 09:49:24 +0100

    Added dummy contact-info

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -11,6 +11,13 @@
           <a href="#">Contact</a>
         </nav>
       </div>
+      <div>
+        <p>Dummy Contact Info</p>
+        <p>Name: John Doe</p>
+        <p>Address: 123 Main St</p>
+        <p>Phone: (123) 456-7890</p>
+        <p>Email: john.doe@example.com</p>
+      </div>
     </div>
   </footer>
 </template>
