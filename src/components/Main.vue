aa797cf0491a3a3c1f0796d68a2bd84ef08bec93
PrimaryV1C <petrovictorelias@gmail.com>
2024-12-11 09:48:21 +0100

    Created Main.vue

---
+++ src/components/Main.vue
@@ -0,0 +1,13 @@
+<template>
+  <main>
+    <div>
+      <p>This is where your main content will be displayed.</p>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "Main",
+};
+</script>
