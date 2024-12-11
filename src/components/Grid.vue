7b26d9648a383eb9ea59565d8c2bcf7858488fec
PrimaryV1C <petrovictorelias@gmail.com>
2024-12-11 11:42:38 +0100

    Created Grid.vue

---
+++ src/components/Grid.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div class="grid">
+      <div
+          v-for="item in gridItems"
+          :key="item.id"
+          class="card"
+          @click="navigateTo(item.url)"
+      >
+        <img :src="item.image" :alt="item.title" />
+        <h3>{{ item.title }}</h3>
+        <p>{{ item.text }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GridComponent",
+  methods: {
+    navigateTo(url) {
+      window.location.href = url;
+    },
+  },
+};
+</script>
