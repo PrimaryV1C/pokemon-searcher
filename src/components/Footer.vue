0424ca0f51653a9c9d8bc677531a6a1bae00a1dd
PrimaryV1C <petrovictorelias@gmail.com>
2024-12-11 09:49:05 +0100

    Created Footer.vue

---
+++ src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <footer>
+    <div>
+      <div>
+        <img src="./src/assets/logo.svg" alt="Logo" />
+      </div>
+      <div>
+        <nav>
+          <a href="#">Home</a>
+          <a href="#">About</a>
+          <a href="#">Contact</a>
+        </nav>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
